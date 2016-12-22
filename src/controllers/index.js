fe4de79e64931ac01e6398750114a00a04e7aa30
sedat sevgili <sedatsevgili@gmail.com>
2016-12-23 01:59:09 +0300

    implemented renewing last invalid token

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,43 +1,28 @@
-var moment = require('moment');
+
 var client = require('../modules/api/client');
+var timeFilter = require('../modules/filters/post/timeFilter');
 
 exports.getIndex = function(req, res) {
     client.getPosts(function(err, responseData) {
         if(err) {
-            return res.err(err);
+            return handleError(err, res);
         }
-        console.log(responseData);
-    });
+        timeFilter.filter(responseData, function(err, posts) {
+            if(err) {
+                return handleError(err, res);
+            }
+            res.render('pages/index', {
+                'posts': posts,
+                'now': new Date()
+            });
+        });
+    });    
+}
 
-    var posts = [
-       {
-           'content': 'We should have AccessToken and RefreshToken mongoose schemas. Then we should have an oauth2 client such as http://lelylan.github.io/simple-oauth2/ and a rest client like https://www.npmjs.com/package/node-rest-client. We have to combined all of them in one single module.',
-           'tags': [
-               {
-                   'name': 'tag-1'
-               },
-               {
-                   'name': 'tag-2'
-               }
-           ],
-           'createdAt': moment(new Date()).format('YYYY-MM-DD HH:mm:ss')
-       },
-       {
-           'content': 'content-3',
-           'tags': [
-               {
-                   'name': 'tag-3'
-               }
-           ],
-           'createdAt': moment(new Date()).format('YYYY-MM-DD HH:mm:ss')
-       },
-       {
-           'content': 'selam',
-           'createdAt': moment(new Date()).format('YYYY-MM-DD HH:mm:ss')
-       }
-   ];
-    res.render('pages/index', {
-        'posts': posts,
+function handleError(err, res) {
+    return res.render('pages/error', {
+        'errorMessage': err,
         'now': new Date()
     });
 }
+
--- src/modules/api/client.js
+++ src/modules/api/client.js
@@ -1,23 +1,45 @@
 var Token = require('../../models/token');
 var oauth2Module = require('simple-oauth2');
+var restClient = require('node-rest-client').Client;
 
 const credentials = {
     client: {
-        id: '123', //process.env.JAMB_API_CLIENT_ID,
-        secret: '456', //process.env.JAMB_API_CLIENT_SECRET
+        id: process.env.JAMB_API_CLIENT_ID,
+        secret: process.env.JAMB_API_CLIENT_SECRET
     },
     auth: {
-        tokenHost: 'http://localhost:3000', //process.env.JAMB_API_HOST,
+        tokenHost: process.env.JAMB_API_HOST,
         tokenPath: '/oauth2/token'
     }
 };
 
 var oauth2 = oauth2Module.create(credentials);
 
+loadLastToken = function(callback) {
+    Token.findOne({}, function(err, token) {
+        if(err) {
+            return callback(err);
+        }
+        return callback(false, token);
+    }).sort({'_id': 1});
+}
+
+deleteLastTokenAndCreateNewOne = function(callback) {
+    loadLastToken(function(err, token) {
+        if(err) {
+            return callback(err);
+        }
+        if(token) {
+            token.remove(loadLastToken(callback));
+        }
+        loadLastToken(callback);
+    });
+}
+
 // fetches last token from storage
 // if there is no token, creates one.
 getToken = function(callback) {
-    Token.findOne({}, function(err, token) {
+    loadLastToken(function(err, token) {
         if(err) {
             return callback(err);
         }
@@ -25,16 +47,16 @@ getToken = function(callback) {
             return createToken(callback);
         }
         var tokenObject = oauth2.accessToken.create(token);
-        if(token.expired()) {
-            token.refresh((err, result) => {
+        if(tokenObject.expired()) {
+            tokenObject.refresh((err, result) => {
                 if(err) {
                     return callback(err);
                 }
-                saveToken(false, result, callback);
+                saveToken(false, result);
             });
         }
         return callback(false, token);
-    }).sort({'_id': 1});
+    });
 }
 
 // creates a valid token for api
@@ -46,9 +68,9 @@ createToken = function(callback) {
 }
 
 // 
-saveToken = function(err, result, callback) {
+saveToken = function(err, result) {
     if(err) {
-        return callback(err);
+        return console.error('Access Token Error', err.message);
     }
     createdTokenObject = oauth2.accessToken.create(result);
     tokenModelToSave = new Token();
@@ -57,9 +79,8 @@ saveToken = function(err, result, callback) {
     tokenModelToSave.expires_in = createdTokenObject.token.expires_in;
     tokenModelToSave.save(function(err) {
         if(err) {
-            return callback(err);
+            console.error('Could not save token', err.message);
         }
-        callback(false, tokenModelToSave);
     });
 }
 
@@ -71,6 +92,17 @@ exports.getPosts = function(callback) {
         if(!token) {
             return callback('auth error!');
         }
-        // todo use token to get resources
+        var client = new restClient();
+        client.get(process.env.JAMB_API_HOST + '/posts', {
+            headers: {'Authorization': 'Bearer ' + token.access_token}
+        }, function(data, response) {
+            if(response.statusCode == 401) {
+                deleteLastTokenAndCreateNewOne(exports.getPosts(callback));
+            }
+            if(response.statusCode != 200) {
+                callback(response.statusMessage);
+            }
+            callback(false, data);
+        });
     });
 }
