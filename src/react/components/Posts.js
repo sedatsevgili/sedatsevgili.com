143bb471dbac5757ae19d12d1f630ea0f878527b
sedat sevgili <sedatsevgili@gmail.com>
2017-01-19 18:15:07 +0300

    made Posts react component work with axios

--- src/react/components/Posts.js
+++ src/react/components/Posts.js
@@ -1,22 +1,36 @@
 import React from 'react';
+import axios from 'axios';
 
 export default class Posts extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            posts: []
+        };
+    }
+
+    componentDidMount() {
+        axios.get('/posts').then(res => {
+            this.setState({posts: res.data.posts});
+        });
+    }
+
     render() {
 
         return (
             <div className="container">
                 <div className="row">
                     <div className="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-                        {/* <% posts.forEach(function(post) { %> */}
-                        <div className="post-preview">
-                            <a>
-                                <h2 className="post-subtitle">
-                                    Hello World!
-                                    {/* <%= post.content %> */}
-                                </h2>
-                            </a>
-                            <p className="post-meta">Posted at NOW! {/* <%= post.createdAt %> */}</p>
-                        </div>    
+                        {this.state.posts.map(post => 
+                            <div className="post-preview">
+                                <a>
+                                    <h2 className="post-subtitle">
+                                        {post.content}
+                                    </h2>
+                                </a>
+                                <p className="post-meta">{post.createdAt}</p>
+                            </div>
+                        )}
                         <hr />
                         {/*<% }); %>
                         <!-- Pager -->
