3c697cb6dfa016c3be86d123dc7a64d4fedf5853
sedat sevgili <sedatsevgili@gmail.com>
2017-01-19 08:42:37 +0300

    installed axios and moved posts endpoint

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,27 +1,3 @@
-
-var client = require('../modules/api/client');
-var timeFilter = require('../modules/filters/post/timeFilter');
-
 exports.getIndex = function(req, res) {
-    client.getPosts([]).then(function(responseData) {
-        timeFilter.filter(responseData, function(err, posts) {
-            if(err) {
-                return handleError(err, res);
-            }
-            res.render('pages/index', {
-                'posts': posts,
-                'now': new Date()
-            });
-        });
-    }, function(err) {
-        return handleError(err, res);
-    });
+    return res.render('pages/index');
 }
-
-function handleError(err, res) {
-    return res.render('pages/error', {
-        'errorMessage': err,
-        'now': new Date()
-    });
-}
-
--- src/server.js
+++ src/server.js
@@ -3,11 +3,13 @@ var mongoose = require('mongoose');
 var app = express();
 var router = express.Router();
 var indexController = require('./controllers/index');
+var postController = require('./controllers/post');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 router.route('/').get(indexController.getIndex);
+router.route('/posts').get(postController.getPosts);
 app.use('/', router);
 
 mongoose.connect(process.env.SS_MONGODB_URI || 'mongodb://localhost:27017/ss');
