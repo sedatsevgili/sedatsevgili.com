52c6a399eace033720403069f8aef5e081571e87
sevgilisedat <sedatsevgili@gmail.com>
2018-05-10 00:27:52 +0300

    fixes redundant links in footer

--- src/react/components/Footer.js
+++ src/react/components/Footer.js
@@ -10,8 +10,7 @@ export default class Footer extends React.Component {
                         <div className="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
                             <ul className="list-inline text-center">
                                 <FooterLink link="https://twitter.com/sedatsevgili" icon="fa-twitter" />
-                                <FooterLink link="https://www.facebook.com/sedat.sevgili" icon="fa-facebook" />
-                                <FooterLink link="https://github.com/sevgilisedat" icon="fa-github" />
+                                <FooterLink link="https://github.com/sedatsevgili" icon="fa-github" />
                             </ul>
                             <p className="copyright text-muted">Copyleft &copy; sedatsevgili.com</p>
                         </div>
